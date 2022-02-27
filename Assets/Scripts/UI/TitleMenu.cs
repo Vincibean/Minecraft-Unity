03b4e0d7f8e0913948122c8aed70a0edf3737ba9
Vincibean <sepensi@gmail.com>
2022-02-27 22:39:33 +0100

    bug fixing

--- Assets/Scripts/UI/TitleMenu.cs
+++ Assets/Scripts/UI/TitleMenu.cs
@@ -50,7 +50,7 @@ public class TitleMenu : MonoBehaviour {
     public void StartGame() {
 
         VoxelData.seed = Mathf.Abs(seedField.text.GetHashCode()) / VoxelData.WorldSizeInChunks;
-        SceneManager.LoadScene("main", LoadSceneMode.Single);
+        SceneManager.LoadScene("World", LoadSceneMode.Single);
 
     }
 
